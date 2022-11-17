af699f4756f2d49b5258cae96572abdbd54e8c38
wyne-ybanez <wyne_01@icloud.com>
2022-11-17 16:04:01 +0000

    Learning: Add comments to code - notes

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,22 @@ const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
+  // Where webpack enters to find your files
   entry: {
-    bundle: path.resolve(__dirname, 'src/index.js'),
+    bundle: path.resolve(__dirname, "src/index.js"),
   },
+  // Where webpack will output your bundled files
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name][contenthash].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
     clean: true,
-    assetModuleFilename: '[name][ext]',
+    assetModuleFilename: "[name][ext]",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, "dist"),
     },
     port: 3000,
     open: true,
@@ -25,34 +27,40 @@ module.exports = {
     compress: true,
     historyApiFallback: true,
   },
+  // Terminal Command: npm i -D sass style-loader css-loader sass-loader
   module: {
     rules: [
       {
+        // any file that ends with .scss - we are going to apply these loaders
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
+  /*
+   When build runs, injects template into index.html.
+   You can find the template in the src folder
+  */
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack App',
-      filename: 'index.html',
-      template: 'src/template.html',
+      title: "Webpack App",
+      filename: "index.html",
+      template: "src/template.html",
     }),
     new BundleAnalyzerPlugin(),
   ],
-}
+};
